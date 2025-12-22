dd9638820962b922b3814cde7e7754ffad54f037
Siddharth Karunakaran <siddharthkarunakaran@gmail.com>
2025-12-22 03:09:37 -0500

    Created the docker compose and an init file that creates the database tables for the data

---
+++ postgres/init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE housing.public.states
+(
+    id        serial PRIMARY KEY,
+    name      varchar(13) NOT NULL,
+    code      varchar(2)  NOT NULL,
+    fips_code integer     NOT NULL,
+    UNIQUE (code, fips_code)
+);
+
+CREATE TABLE housing.public.county
+(
+    id        serial PRIMARY KEY,
+    state_id  integer NOT NULL REFERENCES housing.public.states (id),
+    name      varchar(30),
+    fips_code varchar(4),
+    UNIQUE (name, fips_code)
+);
+
+CREATE TABLE housing.public.debt
+(
+    id        serial PRIMARY KEY,
+    date      date,
+    low       float4,
+    high      float4,
+    county_id integer NOT NULL REFERENCES housing.public.county (id)
+        CONSTRAINT debt_quarter_start_chk
+            CHECK (
+                EXTRACT(MONTH FROM date) IN (1, 4, 7, 10)
+                    AND EXTRACT(DAY FROM date) = 1
+                )
+)
