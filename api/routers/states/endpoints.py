87e46d27e1e053f473644199e5edd1ca9ba89218
Siddharth Karunakaran <siddharthkarunakaran@gmail.com>
2025-12-25 14:09:50 -0500

    add a 404 error

--- api/routers/states/endpoints.py
+++ api/routers/states/endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from starlette.exceptions import HTTPException
 
 from api.di import get_states_service
 from api.routers.states.models import State
@@ -15,6 +16,13 @@ router = APIRouter(
     summary="Get state information",
     description="Get all state information or specific state info by query params",
     status_code=200,
+    responses={
+        404: {"detail": "No State data found"},
+    }
 )
 async def states(service: StatesService = Depends(get_states_service), name: str | None = None, code: str | None = None, fips_code: str | None = None) -> list[State]:
-    return await service.get_states(name, code, fips_code)
+    response = await service.get_states(name, code, fips_code)
+
+    if len(response) == 0:
+        raise HTTPException(status_code=404, detail="No State data found")
+    return response
